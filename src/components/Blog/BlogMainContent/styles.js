c855c10e33fc8a38f237bbdc4c199605a37bae21
Andre <als.andresilveira@gmail.com>
2020-09-29 16:11:09 -0300

    update responsive to mobile

--- src/components/Blog/BlogMainContent/styles.js
+++ src/components/Blog/BlogMainContent/styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
+import media from 'styled-media-query'
 
 export const BlogWrapper = styled.div`
   display:flex;
-  flex-direction:column;
-  align-items:center;
+
 `;
